2f29055218ea53fad3596725d204ef303ff3722a
Harsha  S J <harsha.sj@collabera.com>
2022-06-21 16:45:06 +0530

    creatable select

--- demo-app/src/components/modals/Modals.js
+++ demo-app/src/components/modals/Modals.js
@@ -12,29 +12,30 @@ const createOption = (label) => ({
 const Modals = () => {
   const [inputValue, setInputValue] = useState("");
   const [value, setValue] = useState([]);
-  let handleChange = (value, actionMeta) => {
-    console.group("Value Changed");
-    console.log(value);
-    console.log(`action: ${actionMeta.action}`);
-    console.groupEnd();
-    setValue(value);
-  };
-  let handleInputChange = (inputValue) => {
-    setInputValue(inputValue);
-  };
-  let handleKeyDown = (event) => {
-    if (!inputValue) return;
-    switch (event.key) {
-      case "Enter":
-      case "Tab":
-        console.group("Value Added");
-        console.log(value);
-        console.groupEnd();
-        setValue([...value, createOption(inputValue)]);
-        setInputValue("");
-        event.preventDefault();
-    }
-  };
+
+  // let handleChange = (value, actionMeta) => {
+  //   console.group("Value Changed");
+  //   console.log(value);
+  //   console.log(`action: ${actionMeta.action}`);
+  //   console.groupEnd();
+  //   setValue(value);
+  // };
+  // let handleInputChange = (inputValue) => {
+  //   setInputValue(inputValue);
+  // };
+  // let handleKeyDown = (event) => {
+  //   if (!inputValue) return;
+  //   switch (event.key) {
+  //     case "Enter":
+  //     case "Tab":
+  //       console.group("Value Added");
+  //       console.log(value);
+  //       console.groupEnd();
+  //       setValue([...value, createOption(inputValue)]);
+  //       setInputValue("");
+  //       event.preventDefault();
+  //   }
+  // };
 
   return (
     <div>
@@ -45,15 +46,16 @@ const Modals = () => {
         }}
       />
       <CreatableSelect
-        components={components}
-        inputValue={inputValue}
+        // components={components}
+        // inputValue={inputValue}
         isClearable
         isMulti
         menuIsOpen={false}
-        onChange={handleChange}
-        onInputChange={handleInputChange}
-        onKeyDown={handleKeyDown}
-        placeholder="Type something and press enter..."
+        // onChange={handleChange}
+        // onInputChange={handleInputChange}
+        // onKeyDown={handleKeyDown}
+        // isDisabled
+        placeholder=""
         value={value}
       />
     </div>
